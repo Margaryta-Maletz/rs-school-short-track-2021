782faa053a6870bc1bb0777698ebece47230561c
Margaryta-Maletz <rita.maletz@gmail.com>
2021-04-09 00:48:39 +0300

    feat: add solution for 11-encode-line

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,21 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  let resStr = '';
+  let i = 0;
+  while (i < arr.length) {
+    let count = 1;
+    while (arr[i] === arr[i + 1]) {
+      i++;
+      count++;
+    }
+    resStr += count === 1 ? '' : count;
+    resStr += arr[i];
+    i++;
+  }
+  return resStr;
 }
 
 module.exports = encodeLine;
