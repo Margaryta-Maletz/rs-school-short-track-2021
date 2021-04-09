52da46c406298cc469985ff0df96fb321941aad6
Margaryta-Maletz <rita.maletz@gmail.com>
2021-04-09 20:41:23 +0300

    feat: add solution for 07-file-names

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,23 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const res = [];
+  for (let i = 0; i < names.length; i++) {
+    let str = names[i];
+    const temp = i > 0 ? names.slice(0, i) : [];
+    let len = temp.filter((item) => item === str).length;
+    if (len > 0) {
+      str = str.concat('(', len, ')');
+    } else {
+      len = res.filter((item) => item === str).length;
+      if (len > 0) {
+        str = str.concat('(', len, ')');
+      }
+    }
+    res.push(str);
+  }
+  return res;
 }
 
 module.exports = renameFiles;
